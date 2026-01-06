e8dc512a1f625de1cf46837bc39b8907b5fb5489
Santu Yadav <santu.yadav2503@gmail.com>
2026-01-06 20:05:24 +0530

    basic component - footer, header, screen implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,20 @@
-function App() {
-  return <>Hello</>;
-}
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreens from "./screens/HomeScreens";
 
+function App() {
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          
+          <HomeScreens />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+}
 export default App;
