8b26e3048668cc68de05fe7d8c7af0f108384a00
Santu Yadav <santu.yadav2503@gmail.com>
2026-08-01 18:00:18 +0530

    fix: recalculate cart totals after removing items

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -13,8 +13,21 @@ const cartSlice = createSlice({
       // The item to add to the cart
       const item = action.payload;
 
+      // Check if the item is already in the cart
+      const existItem = state.cartItems.find((x) => x._id === item._id);
+
+      if (existItem) {
+        // If exists, update quantity
+        state.cartItems = state.cartItems.map((x) =>
+          x._id === existItem._id ? item : x,
+        );
+      } else {
+        // If not exists, add new item to cartItems
+        state.cartItems = [...state.cartItems, item];
+      }
+
       // Update the cart state using the updateCart function
-      return updateCart(state, item);
+      return updateCart(state);
     },
 
     removeFromCart: (state, action) => {
@@ -23,7 +36,8 @@ const cartSlice = createSlice({
 
       //update the cart state
       state.cartItems = state.cartItems.filter((item) => item._id !== id);
-      localStorage.setItem("cart", JSON.stringify(state));
+      return updateCart(state);
+      // localStorage.setItem("cart", JSON.stringify(state));
     },
   },
 });
--- frontend/src/utils/cartUtils.js
+++ frontend/src/utils/cartUtils.js
@@ -2,27 +2,17 @@ export const addDecimals = (num) => {
   return (Math.round(num * 100) / 100).toFixed(2);
 };
 
-export const updateCart = (state, item) => {
-  // Check if the item is already in the cart
-  const existItem = state.cartItems.find((x) => x._id === item._id);
-
-  if (existItem) {
-    // If exists, update quantity
-    state.cartItems = state.cartItems.map((x) =>
-      x._id === existItem._id ? item : x,
-    );
-  } else {
-    // If not exists, add new item to cartItems
-    state.cartItems = [...state.cartItems, item];
-  }
-
+export const updateCart = (state) => {
   // Calculate the items price
   state.itemsPrice = addDecimals(
     state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0),
   );
 
+  console.log("state.itemsPrice @@ :", state.itemsPrice);
   // Calculate the shipping price
-  state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+  state.shippingPrice = addDecimals(
+    state.itemsPrice > 0 && state.itemsPrice <= 100 ? 10 : 0,
+  );
 
   // Calculate the tax price
   state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)));
