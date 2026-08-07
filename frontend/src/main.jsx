551a03348d54c93d1d100b2a4e569dc64207c31a
Santu Yadav <santu.yadav2503@gmail.com>
2026-08-07 23:44:50 +0530

    feat(orders): final order page completed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,6 +13,7 @@ import ShippingScreen from "./screens/shippingScreen.jsx";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen.jsx";
 import PrivateRoute from "./components/PrivateRoute.jsx";
 import Payment from "./screens/PaymentScreen.jsx";
+import OrderScreen from "./screens/OrderScreen.jsx";
 
 import store from "./store";
 import { Provider } from "react-redux";
@@ -31,6 +32,7 @@ createRoot(document.getElementById("root")).render(
             <Route path="/shipping" element={<ShippingScreen />} />
             <Route path="/payment" element={<Payment />} />
             <Route path="/placeOrder" element={<PlaceOrderScreen />} />
+            <Route path="/order/:id" element={<OrderScreen />} />
           </Route>
         </Route>
       </Routes>
