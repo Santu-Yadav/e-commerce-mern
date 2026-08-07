8ddcbe950bc91a2affbdb83df9f9fcee703cbf46
Santu Yadav <santu.yadav2503@gmail.com>
2026-08-07 23:49:08 +0530

    feat: Add expiration time logic to front end - App.js and auth slice"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,6 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
 import { Container } from "react-bootstrap";
 import { Outlet } from "react-router";
 
@@ -6,8 +9,21 @@ import "react-toastify/dist/ReactToastify.css";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { logout } from "./slices/authSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const expirationTime = localStorage.getItem("expirationTime");
+    if (expirationTime) {
+      const currentTime = new Date().getTime();
+      if (currentTime > expirationTime) {
+        dispatch(logout());
+      }
+    }
+  }, [dispatch]);
+
   return (
     <>
       <ToastContainer />
